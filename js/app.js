bec9d98e97ebab4a36220fe3bf7ad250f7440841
Oliver Critchfield <oliver.critchfield@gmail.com>
2018-06-06 05:56:14 +0100

    Added function to shuffle and redeal the cards

--- js/app.js
+++ js/app.js
@@ -1,7 +1,15 @@
 /*
  * Create a list that holds all of your cards
  */
-
+const cards = ['fa-diamond', 'fa-diamond',
+               'fa-paper-plane-o', 'fa-paper-plane-o',
+               'fa-anchor', 'fa-anchor',
+               'fa-bolt', 'fa-bolt',
+               'fa-cube', 'fa-cube',
+               'fa-bicycle', 'fa-bicycle',
+               'fa-leaf', 'fa-leaf',
+               'fa-bomb', 'fa-bomb',
+               ];
 
 /*
  * Display the cards on the page
@@ -12,7 +20,7 @@
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+    let currentIndex = array.length, temporaryValue, randomIndex;
 
     while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
@@ -25,6 +33,23 @@ function shuffle(array) {
     return array;
 }
 
+// A function to set up the page
+// Shuffles the deck and lays the shuffled cards in order
+function shuffleCards() {
+  shuffle(cards);
+  let deckHTML = document.createElement('ul');
+  deckHTML.classList.add('deck');
+  let deckHTMLToAdd = '';
+  cards.forEach(function(card) {
+    newCard = `<li class="card"><i class="fa ${card}"></i></li>`
+    deckHTMLToAdd = deckHTMLToAdd + newCard;
+  });
+  deckHTML.insertAdjacentHTML('beforeEnd', deckHTMLToAdd);
+  const oldCards = document.querySelector('.deck');
+  oldCards.replaceWith(deckHTML);
+};
+
+ShuffleCards();
 
 /*
  * set up the event listener for a card. If a card is clicked:
@@ -44,6 +69,8 @@ let moves = 0;
 
 allCards.forEach(function(card) {
   card.addEventListener('click', function(evt) {
+    // If card is not turned yet, turn and add to list of open cards
+    // Increment and update the move counter
     if (!card.classList.contains('open') && !card.classList.contains('show') && !card.classList.contains('match')) {
       card.classList.add('open', 'show');
       openCards.push(card);
@@ -55,18 +82,24 @@ allCards.forEach(function(card) {
         moveCounter.textContent = moves + ' Moves';
       };
     };
+    // If two cards have been turned check whether they match
     if (openCards.length >= 2) {
-      setTimeout(function () {
+      setTimeout(function (){
         openCards.forEach(function(card) {
           card.classList.remove('open', 'show');
         });
         // If cards match leave unturned and change background color
-        if (openCards[0].childNodes[1].classList.value === openCards[1].childNodes[1].classList.value) {
+        console.log(openCards[0].querySelector('.fa'));
+        if (openCards[0].querySelector('.fa').classList.value === openCards[1].querySelector('.fa').classList.value) {
           openCards.forEach(function(card) {
             card.classList.add('match');
           });
         }
-
+        else {
+          openCards.forEach(function(card) {
+            card.classList.add('no_match');
+          });
+        };
         openCards = [];
       }, 500);
     };
